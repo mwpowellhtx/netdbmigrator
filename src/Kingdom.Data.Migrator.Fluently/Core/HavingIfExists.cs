e482d88b86aa78083c3de402c0b08f5b1857dad5
Michael W Powell <mwpowellhtx@gmail.com>
2016-03-16 18:34:46 -0400

    added clarifying comments to the if exists changes does not apply for sql server versions through 2012 is a new clause added to 2014 and beyond should be fairly transparent for use throughout the fluent framework

--- src/Kingdom.Data.Migrator.Fluently/Core/HavingIfExists.cs
+++ src/Kingdom.Data.Migrator.Fluently/Core/HavingIfExists.cs
@@ -3,6 +3,8 @@
     /// <summary>
     /// Represents whether Having If Exists.
     /// </summary>
+    /// <see cref="!:http://msdn.microsoft.com/en-us/library/ms190273.aspx" >ALTER TABLE (Transact-SQL), Sql Server 2014+</see>
+    /// <remarks>Does not apply for versions through Sql Server 2012.</remarks>
     public interface IHavingIfExists
     {
         /// <summary>
--- src/Kingdom.Data.Migrator.Fluently/Core/IAlterIfExists.cs
+++ src/Kingdom.Data.Migrator.Fluently/Core/IAlterIfExists.cs
@@ -4,6 +4,8 @@ namespace Kingdom.Data
     /// Signals to render If Exists clause in the statement.
     /// </summary>
     /// <typeparam name="TParent"></typeparam>
+    /// <see cref="!:http://msdn.microsoft.com/en-us/library/ms190273.aspx" >ALTER TABLE (Transact-SQL), Sql Server 2014+</see>
+    /// <remarks>Does not apply for versions through Sql Server 2012.</remarks>
     public interface IAlterIfExists<out TParent>
         where TParent : IAlterIfExists<TParent>
     {
